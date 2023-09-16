367662927fc0e9aefaffa72eafd594066e8e5967
MichielsSFH <s.f.h.michiels@student.tue.nl>
2023-09-17 00:42:25 +0200

    Fixed ItemGroupRegister Typos

--- src/main/java/net/fabricmc/streetparts/register/ModItemGroup.java
+++ src/main/java/net/fabricmc/streetparts/register/ModItemGroup.java
@@ -161,4 +161,7 @@ public class ModItemGroup {
                         entries.add(ModBlocks.DIRECTION_SIGN);
 
                     }).build());
+    public static void registerItemGroups() {
+        StreetParts.LOGGER.info("Registering Item Groups for " + StreetParts.MOD_ID);
+    }
 }
